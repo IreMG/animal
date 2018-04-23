b55c0753fa65fe689b16b573287e4be60617a2d1
ire <ire@Ire>
2018-04-23 11:48:31 +0200

    Añadir constructor,inicializar velocidad

--- Animal/src/Animal.java
+++ Animal/src/Animal.java
@@ -3,16 +3,25 @@ public class Animal {
 	private String raza;
 	private String nombre;
 	private int edad;
-	private int velocidad=0;
+	private int velocidad;
 	private String sexo;
 	
+	public Animal(String raza, String nombre, int edad, int velocidad, String sexo) {
+		super();
+		this.raza = raza;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.velocidad =0;
+		this.sexo = sexo;
+	}
+
 	public int Acelerar() {
 		if (raza=="felino") {
-		velocidad+=20;
+		this.velocidad+=20;
 		}
 		else {
-			velocidad+=5;
+			this.velocidad+=5;
 		}
-		return velocidad;
+		return this.velocidad;
 	}
 }
